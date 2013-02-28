810bc144a39bd1178ad255d4bb90a0d77daa30c3
Zack Krejci <zack.krejci@gmail.com>
2013-02-28 15:07:54 -0500

    interp between points on line - still hacky, but better

--- src/query/TimeSeriesQuerier.js
+++ src/query/TimeSeriesQuerier.js
@@ -1,15 +1,39 @@
 var TimeSeriesQuerier = function (engine, layer, options) {
     var r_points = [];
     layer.features ().each (function (n, polygon) {
+        var pushPoint = function (v) {
+            r_points.push ({
+                ref: polygon,
+	        x: v.x,
+	        y: v.y
+            });
+        };
 	$.each (options.geomFunc (polygon), function (i, poly) {
 	    $.each (poly, function (j, ring) {
-		$.each (ring, function (k, pair) {
+                var currentPoint = new vect (ring[0][0], ring[0][1]);
+                pushPoint (currentPoint);
+                for (var k = 1; k < ring.length; k ++) {
+                    var nextPoint = new vect (ring[k][0], ring[k][1]);
+                    var t = 0;
+                    while (t < 1) {
+                        t += .1;
+                        var dir = vect.sub (nextPoint, currentPoint).scale (t);
+                        pushPoint (vect.add (currentPoint, dir));
+                    }
+                    currentPoint = nextPoint;
+                }
+		/*$.each (ring, function (k, pair) {
+                    var t = 0;
+                    var min 
+                    while (t < 1) {
 		    r_points.push ({
                         ref: polygon,
 			x: pair[0],
 			y: pair[1]
 		    });			
-		});
+                        t += .1;
+                    }
+		});*/
 	    });
 	});
     });
