3528048279ed99058cf5eb3a179b0b9e671ffd5e
Zack Krejci <zack.krejci@gmail.com>
2013-05-10 13:13:26 -0400

    append bug

--- src/Map.js
+++ src/Map.js
@@ -125,7 +125,7 @@ var Map = function (selector, options) {
     // event and style registrations) and a set of queriers (that
     // handle searching rendered geometry)
     this.append = function (layer) {
-        this.insert(layer, this.scene.length - 1);
+        this.insert(layer, this.scene.length);
     };
 
     this.insert = function(layer, index) {
